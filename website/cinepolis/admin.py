d5babc92887d29cc67fc3668a2ae5f2466af569d
Sergio Adonis <sergio.adonis@hotmail.com>
2021-03-08 20:25:02 -0600

    Billboard request and movie models

--- website/cinepolis/admin.py
+++ website/cinepolis/admin.py
@@ -2,11 +2,29 @@ from django.contrib import admin
 from . import models
 
 
-class CinemaInlineAdmin(admin.TabularInline):
-    model = models.Cinema
+@admin.register(models.Cinema)
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ['cinema_name', 'cinema_number']
 
 
 @admin.register(models.City)
 class CityAdmin(admin.ModelAdmin):
     list_display = ['city_name', 'city_number']
-    inlines = [CinemaInlineAdmin]
+
+
+class BillboardMovieInline(admin.TabularInline):
+    model = models.BillboardMovie
+    can_delete = False
+
+    def has_add_permission(*args, **kwargs):
+        return False
+
+    def has_change_permission(*args, **kwargs):
+        return False
+
+
+@admin.register(models.BillboardRequest)
+class BillboardRequestAdmin(admin.ModelAdmin):
+    list_display = ['city', 'date', 'status']
+    inlines = [BillboardMovieInline]
+    readonly_fields = ['status', 'response']
--- website/cinepolis/models.py
+++ website/cinepolis/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
 
 
 class City(models.Model):
@@ -13,9 +14,42 @@ class City(models.Model):
 
 
 class Cinema(models.Model):
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
     cinema_name = models.CharField(max_length=50)
     cinema_number = models.PositiveIntegerField()
 
     def __str__(self):
         return self.cinema_name
+
+
+class RequestStatus(models.TextChoices):
+    TO_DO = 'TO_DO'
+    DOING = 'DOING'
+    DONE = 'DONE'
+    ERROR = 'ERROR'
+
+
+class BillboardRequest(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(
+        max_length=5, choices=RequestStatus.choices, default=RequestStatus.TO_DO)
+    response = models.JSONField(default=list)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.date} - {self.city}'
+
+
+class BillboardMovie(models.Model):
+    billboard_request = models.ForeignKey(
+        BillboardRequest, on_delete=models.CASCADE)
+    movie_number = models.PositiveIntegerField()
+    movie_title = models.CharField(max_length=50)
+    cinemas = models.CharField(max_length=50,
+                               validators=[
+                                   validate_comma_separated_integer_list],
+                               default='')
+
+    def __str__(self):
+        return self.movie_title
